70d8eaf1cfda4ee3a8584f597f0660849cad1b60
al-burlachenko <alexandrburl@gmail.com>
2025-12-11 14:43:27 +0200

    let's make it simple with js, can't go wrong with it

--- src/components/App/App.tsx
+++
@@ -1,8 +0,0 @@
-import BirthdayGreetingSite from '../BirthdayGreetingSite/BirthdayGreetingSite';
-import './App.css';
-
-function App() {
-  return <BirthdayGreetingSite />;
-}
-
-export default App;
